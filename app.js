412c6a8f7314598945408e401c233e66d32f57c9
edamago <daniel.martel@outlook.com>
2024-04-28 16:02:50 -0500

    se conectó a mongodb

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ const port= process.env.PORT || 3000;
 // Conexion a base de datos
 const mongoose = require('mongoose');
 
+const user = "edamago"
+const password = "q27NUoQAa78A2kSy"
+const dbname = "veterinaria"
 
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.sbhm3a3.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority&appName=Cluster0`;
-
+//const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.sbhm3a3.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${user}:${password}@cluster0.sbhm3a3.mongodb.net/${dbname}?retryWrites=true&w=majority&appName=Cluster0`;
 mongoose.connect(uri)
         .then(()=> console.log("Base de datos conectada mongodb"))
         .catch((error)=>console.log(error))
