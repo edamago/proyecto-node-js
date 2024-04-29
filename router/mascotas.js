ebf74137eab9d2ef109c917c7986e5c9157c27fc
edamago <daniel.martel@outlook.com>
2024-04-29 12:30:03 -0500

    Se completó CRUD

--- router/mascotas.js
+++ router/mascotas.js
@@ -87,4 +87,26 @@ router.delete("/:id",async(req,res)=>{
     }
 })
 
+router.put("/:id",async (req,res)=>{
+    const id = req.params.id
+    const body = req.body
+    try {
+        const mascotaDB = await Mascota.findByIdAndUpdate(id, body,{ useFindAndModify:false })
+        console.log(mascotaDB)
+
+        res.json({
+            estado: true,
+            mensaje: "Editado"
+        })
+
+    } catch (error) {
+        console.log(error)
+
+        res.json({
+            estado: false,
+            mensaje: "No Editado"
+        })
+    }
+})
+
 module.exports = router
